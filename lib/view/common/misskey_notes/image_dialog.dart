fb9952bc446d73364f4add98c3433c8df2ab8b5a
sorairo <sorairo@shiosyakeyakini.info>
2023-05-30 20:41:05 +0900

    もう少し改善

--- lib/view/common/misskey_notes/image_dialog.dart
+++ lib/view/common/misskey_notes/image_dialog.dart
@@ -20,6 +20,23 @@ class ImageDialog extends StatefulWidget {
 class ImageDialogState extends State<ImageDialog> {
   var scale = 1.0;
   late final pageController = PageController(initialPage: widget.initialPage);
+  var verticalDragX = 0.0;
+  var verticalDragY = 0.0;
+  int? listeningId;
+  final TransformationController _transformationController =
+      TransformationController();
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        verticalDragX = 0;
+        verticalDragY = 0;
+        listeningId = null;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,24 +52,76 @@ class ImageDialogState extends State<ImageDialog> {
           child: Stack(
             children: [
               Positioned.fill(
-                child: PageView(
-                  controller: pageController,
-                  physics: scale == 1.0
-                      ? const ScrollPhysics()
-                      : const NeverScrollableScrollPhysics(),
-                  children: [
-                    for (final url in widget.imageUrlList)
-                      ScaleNotifierInteractiveViewer(
-                        imageUrl: url,
-                        onScaleChanged: (updatedScale) {
+                  child: Listener(
+                      onPointerDown: (event) {
+                        if (listeningId != null) {
                           setState(() {
-                            scale = updatedScale;
+                            verticalDragX = 0;
+                            verticalDragY = 0;
                           });
-                        },
-                      ),
-                  ],
-                ),
-              ),
+                          listeningId = null;
+                          return;
+                        }
+                        if (scale != 1.0) return;
+                        listeningId = event.pointer;
+                      },
+                      onPointerMove: (event) {
+                        if (listeningId != null) {
+                          setState(() {
+                            verticalDragX += event.delta.dx;
+                            verticalDragY += event.delta.dy;
+                          });
+                        }
+                      },
+                      onPointerUp: (event) {
+                        final angle =
+                            (atan2(verticalDragY, verticalDragX).abs() /
+                                pi *
+                                180);
+                        if (listeningId != null &&
+                            verticalDragY.abs() > 10 &&
+                            (angle > 60 && angle < 120)) {
+                          Navigator.of(context).pop();
+                        } else {
+                          listeningId = null;
+                        }
+                      },
+                      child: GestureDetector(
+                          onDoubleTapDown: (details) {
+                            listeningId = null;
+                            if (scale != 1.0) {
+                              _transformationController.value =
+                                  Matrix4.identity();
+                              scale = 1.0;
+                            } else {
+                              final position = details.localPosition;
+                              _transformationController
+                                  .value = Matrix4.identity()
+                                ..translate(-position.dx * 2, -position.dy * 2)
+                                ..scale(3.0);
+                              scale = 3.0;
+                            }
+                          },
+                          child: Transform.translate(
+                            offset: Offset(verticalDragX, verticalDragY),
+                            child: PageView(
+                              controller: pageController,
+                              physics: scale == 1.0
+                                  ? const ScrollPhysics()
+                                  : const NeverScrollableScrollPhysics(),
+                              children: [
+                                for (final url in widget.imageUrlList)
+                                  ScaleNotifierInteractiveViewer(
+                                    imageUrl: url,
+                                    controller: _transformationController,
+                                    onScaleChanged: (scaleUpdated) =>
+                                        setState(() {
+                                      scale = scaleUpdated;
+                                    }),
+                                  ),
+                              ],
+                            ),
+                          )))),
               Positioned(
                 right: 10,
                 top: 10,
@@ -85,12 +154,13 @@ class ImageDialogState extends State<ImageDialog> {
 
 class ScaleNotifierInteractiveViewer extends StatefulWidget {
   final String imageUrl;
-
+  final TransformationController controller;
   final void Function(double) onScaleChanged;
 
   const ScaleNotifierInteractiveViewer({
     super.key,
     required this.imageUrl,
+    required this.controller,
     required this.onScaleChanged,
   });
 
@@ -101,77 +171,23 @@ class ScaleNotifierInteractiveViewer extends StatefulWidget {
 class ScaleNotifierInteractiveViewerState
     extends State<ScaleNotifierInteractiveViewer> {
   var scale = 1.0;
-  final TransformationController _transformationController =
-      TransformationController();
-  var verticalDragX = 0.0;
-  var verticalDragY = 0.0;
-  int? listeningId;
 
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerDown: (event) {
-        print("event.pointer: ${event.pointer} listeningId: $listeningId");
-        if (listeningId != null) {
-          setState(() {
-            verticalDragX = 0;
-            verticalDragY = 0;
-          });
-          listeningId = null;
-          return;
-        }
-        if (scale != 1.0) return;
-        listeningId = event.pointer;
-      },
-      onPointerMove: (event) {
-        if (listeningId != null) {
-          setState(() {
-            verticalDragX += event.delta.dx;
-            verticalDragY += event.delta.dy;
-          });
-        }
-      },
-      onPointerUp: (event) {
-        final angle = (atan2(verticalDragY, verticalDragX).abs() / pi * 180);
-        if (listeningId != null &&
-            verticalDragY.abs() > 10 &&
-            (angle > 60 && angle < 120)) {
-          Navigator.of(context).pop();
-        }
-      },
-      child: GestureDetector(
-          onDoubleTapDown: (details) {
-            if (scale != 1.0) {
-              _transformationController.value = Matrix4.identity();
-              scale = 1.0;
-              widget.onScaleChanged(scale);
-            } else {
-              final position = details.localPosition;
-              _transformationController.value = Matrix4.identity()
-                ..translate(-position.dx * 2, -position.dy * 2)
-                ..scale(3.0);
-              scale = 3.0;
-              widget.onScaleChanged(scale);
-            }
+    return SizedBox(
+        width: MediaQuery.of(context).size.width * 0.95,
+        height: MediaQuery.of(context).size.height * 0.95,
+        child: InteractiveViewer(
+          // ピンチイン・ピンチアウト終了後の処理
+          transformationController: widget.controller,
+          onInteractionEnd: (details) {
+            scale = widget.controller.value.getMaxScaleOnAxis();
+            widget.onScaleChanged(scale);
           },
-          child: Transform.translate(
-            offset: Offset(verticalDragX, verticalDragY),
-            child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.95,
-                height: MediaQuery.of(context).size.height * 0.95,
-                child: InteractiveViewer(
-                  // ピンチイン・ピンチアウト終了後の処理
-                  transformationController: _transformationController,
-                  onInteractionEnd: (details) {
-                    scale = _transformationController.value.getMaxScaleOnAxis();
-                    widget.onScaleChanged(scale);
-                  },
-                  child: NetworkImageView(
-                    url: widget.imageUrl,
-                    type: ImageType.image,
-                  ),
-                )),
-          )),
-    );
+          child: NetworkImageView(
+            url: widget.imageUrl,
+            type: ImageType.image,
+          ),
+        ));
   }
 }
