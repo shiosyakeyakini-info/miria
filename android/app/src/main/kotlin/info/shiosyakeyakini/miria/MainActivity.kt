64bd685164bd241580a7adc01a5cbd944bc9f654
sorairo <sorairo@shiosyakeyakini.info>
2023-06-03 14:45:47 +0900

    パッケージ名修正、共有対応

---
+++ android/app/src/main/kotlin/info/shiosyakeyakini/miria/MainActivity.kt
@@ -0,0 +1,16 @@
+package info.shiosyakeyakini.miria
+
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (intent.getIntExtra("org.chromium.chrome.extra.TASK_ID", -1) == this.taskId) {
+            this.finish()
+            intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        super.onCreate(savedInstanceState)
+    }
+}
