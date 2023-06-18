28208ef401f5cae9ce10f1d1a6d5d1567ebb6a43
sorairo <sorairo@shiosyakeyakini.info>
2023-06-18 16:04:59 +0900

    カスタム絵文字の表示機能修正

--- lib/view/federation_page/federation_custom_emojis.dart
+++ lib/view/federation_page/federation_custom_emojis.dart
@@ -6,6 +6,7 @@ import 'package:miria/model/misskey_emoji_data.dart';
 import 'package:miria/providers.dart';
 import 'package:miria/view/common/error_detail.dart';
 import 'package:miria/view/common/misskey_notes/custom_emoji.dart';
+import 'package:miria/view/themes/app_theme.dart';
 import 'package:misskey_dart/misskey_dart.dart';
 
 class FederationCustomEmojis extends ConsumerStatefulWidget {
@@ -50,40 +51,68 @@ class FederationCustomEmojisState
         final entry = emojis.entries.toList()[index];
         return ExpansionTile(
           title: Text(entry.key),
+          childrenPadding: EdgeInsets.zero,
           children: [
             for (final element in entry.value)
               SizedBox(
                 width: double.infinity,
-                child: Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Wrap(
-                          crossAxisAlignment: WrapCrossAlignment.center,
-                          children: [
-                            CustomEmoji(
-                              emojiData: CustomEmojiData(
-                                  baseName: element.name,
-                                  hostedName: widget.host,
-                                  url: element.url,
-                                  isCurrentServer: false,
-                                  isSensitive: element.isSensitive),
-                              fontSizeRatio: 2,
-                            ),
-                            const Padding(padding: EdgeInsets.only(left: 10)),
-                            Text(
-                              ":${element.name}:",
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 10, right: 10, top: 5, bottom: 5),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10, right: 10, bottom: 10),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text.rich(TextSpan(children: [
+                            WidgetSpan(
+                                child: CustomEmoji(
+                                  emojiData: CustomEmojiData(
+                                      baseName: element.name,
+                                      hostedName: widget.host,
+                                      url: element.url,
+                                      isCurrentServer: false,
+                                      isSensitive: element.isSensitive),
+                                  fontSizeRatio: 2,
+                                ),
+                                alignment: PlaceholderAlignment.middle),
+                            const WidgetSpan(
+                                child: Padding(
+                                    padding: EdgeInsets.only(left: 10))),
+                            TextSpan(
+                              text: ":${element.name}:",
                               style: Theme.of(context).textTheme.titleMedium,
                             ),
-                          ],
-                        ),
-                        const Padding(padding: EdgeInsets.only(top: 10)),
-                        Text(element.aliases.join(" ")),
-                      ],
+                            if (element.isSensitive)
+                              WidgetSpan(
+                                  child: Container(
+                                decoration: BoxDecoration(
+                                    color: Theme.of(context).primaryColor),
+                                child: Padding(
+                                  padding: EdgeInsets.only(left: 3, right: 3),
+                                  child: Text(
+                                    "センシティブ",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium
+                                        ?.copyWith(color: Colors.white),
+                                  ),
+                                ),
+                              )),
+                          ])),
+                          if (element.aliases.isNotEmpty) ...[
+                            const Padding(padding: EdgeInsets.only(top: 10)),
+                            Text(
+                              element.aliases.join(" "),
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                          ]
+                        ],
+                      ),
                     ),
                   ),
                 ),
