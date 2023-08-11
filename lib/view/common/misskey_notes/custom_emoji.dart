24004a188d423fb6b47482591b10d00b895f0553
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-08-12 08:12:32 +0900

    format

--- lib/view/common/misskey_notes/custom_emoji.dart
+++ lib/view/common/misskey_notes/custom_emoji.dart
@@ -61,37 +61,47 @@ class CustomEmojiState extends ConsumerState<CustomEmoji> {
     switch (emojiData) {
       case CustomEmojiData():
         cachedImage = ConditionalTooltip(
-            isAttachTooltip: widget.isAttachTooltip,
-            message: emojiData.hostedName,
-            child: NetworkImageView(
-                url: resolveCustomEmojiUrl(emojiData.url).toString(),
-                type: ImageType.customEmoji,
-                errorBuilder: (context, e, s) => Text(emojiData.hostedName,
-                    style: TextStyle(
-                        color: Theme.of(context).textTheme.bodyMedium?.color)),
-                loadingBuilder: (context, widget, chunk) => SizedBox(
-                      height: scopedFontSize,
-                      width: scopedFontSize,
-                    ),
-                height: scopedFontSize));
+          isAttachTooltip: widget.isAttachTooltip,
+          message: emojiData.hostedName,
+          child: NetworkImageView(
+            url: resolveCustomEmojiUrl(emojiData.url).toString(),
+            type: ImageType.customEmoji,
+            errorBuilder: (context, e, s) => Text(
+              emojiData.hostedName,
+              style: TextStyle(
+                color: Theme.of(context).textTheme.bodyMedium?.color,
+              ),
+            ),
+            loadingBuilder: (context, widget, chunk) => SizedBox(
+              height: scopedFontSize,
+              width: scopedFontSize,
+            ),
+            height: scopedFontSize,
+          ),
+        );
         break;
       case UnicodeEmojiData():
         cachedImage = SizedBox(
-            width: scopedFontSize,
-            height: scopedFontSize,
-            child: FittedBox(
-                fit: BoxFit.cover,
-                child: Text(
-                  emojiData.char,
-                  style: TextStyle(
-                          color: Theme.of(context).textTheme.bodyMedium?.color)
-                      .merge(AppTheme.of(context).unicodeEmojiStyle),
-                )));
+          width: scopedFontSize,
+          height: scopedFontSize,
+          child: FittedBox(
+            fit: BoxFit.cover,
+            child: Text(
+              emojiData.char,
+              style: TextStyle(
+                color: Theme.of(context).textTheme.bodyMedium?.color,
+              ).merge(AppTheme.of(context).unicodeEmojiStyle),
+            ),
+          ),
+        );
         break;
       case NotEmojiData():
-        cachedImage = Text(emojiData.name,
-            style: TextStyle(
-                color: Theme.of(context).textTheme.bodyMedium?.color));
+        cachedImage = Text(
+          emojiData.name,
+          style: TextStyle(
+            color: Theme.of(context).textTheme.bodyMedium?.color,
+          ),
+        );
         break;
     }
     return cachedImage!;
@@ -103,11 +113,12 @@ class ConditionalTooltip extends StatelessWidget {
   final String message;
   final Widget child;
 
-  const ConditionalTooltip(
-      {super.key,
-      required this.isAttachTooltip,
-      required this.message,
-      required this.child});
+  const ConditionalTooltip({
+    super.key,
+    required this.isAttachTooltip,
+    required this.message,
+    required this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
