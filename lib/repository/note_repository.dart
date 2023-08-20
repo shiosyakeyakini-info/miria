eaa151bfcab1efff6d66ec72a9991fedb69d01e7
sorairo <sorairo@shiosyakeyakini.info>
2023-08-21 06:00:13 +0900

    #180 修正（一番上にあるとノートの続きを見るが機能しない）

--- lib/repository/note_repository.dart
+++ lib/repository/note_repository.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:misskey_dart/misskey_dart.dart';
+
+part 'note_repository.freezed.dart';
+
+@freezed
+class NoteStatus with _$NoteStatus {
+  const factory NoteStatus({
+    required bool isCwOpened,
+    required bool isLongVisible,
+    required bool isReactionedRenote,
+    required bool isLongVisibleInitialized,
+  }) = _NoteStatus;
+}
 
 class NoteRepository extends ChangeNotifier {
   final Misskey misskey;
   final Map<String, Note> _notes = {};
+  final Map<String, NoteStatus> _noteStatuses = {};
 
   NoteRepository(this.misskey);
 
   Map<String, Note> get notes => _notes;
+
+  Map<String, NoteStatus> get noteStatuses => _noteStatuses;
+
+  void updateNoteStatus(
+      String id, NoteStatus Function(NoteStatus status) statusPredicate,
+      {bool isNotify = true}) {
+    _noteStatuses[id] = statusPredicate.call(_noteStatuses[id]!);
+    if (isNotify) notifyListeners();
+  }
 
   void _registerNote(Note note) {
     _notes[note.id] = note.copyWith(
       renote: note.renote ?? _notes[note.renoteId],
       reply: note.reply ?? _notes[note.replyId],
     );
+    _noteStatuses[note.id] ??= const NoteStatus(
+        isCwOpened: false,
+        isLongVisible: false,
+        isReactionedRenote: false,
+        isLongVisibleInitialized: false);
     final renote = note.renote;
     final reply = note.reply;
     if (renote != null) {
