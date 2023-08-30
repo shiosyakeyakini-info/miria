3f4557e7b7b599a530ed0f030bc3708eec9d13fb
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-08-30 22:26:14 +0900

    ハンドルを常に表示する

--- lib/view/settings_page/tab_settings_page/tab_settings_list_page.dart
+++ lib/view/settings_page/tab_settings_page/tab_settings_list_page.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:miria/model/tab_setting.dart';
 import 'package:miria/providers.dart';
 import 'package:miria/router/app_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,35 +7,29 @@ import 'package:miria/view/common/account_scope.dart';
 import 'package:miria/view/common/tab_icon_view.dart';
 
 @RoutePage()
-class TabSettingsListPage extends ConsumerStatefulWidget {
+class TabSettingsListPage extends ConsumerWidget {
   const TabSettingsListPage({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      TabSettingsListPageState();
-}
-
-class TabSettingsListPageState extends ConsumerState<TabSettingsListPage> {
-  void save(List<TabSetting> newTabSetting) {
-    ref.read(tabSettingsRepositoryProvider).save(newTabSetting);
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final tabSettings = ref
-        .watch(tabSettingsRepositoryProvider
-            .select((repository) => repository.tabSettings))
+        .watch(
+          tabSettingsRepositoryProvider
+              .select((repository) => repository.tabSettings),
+        )
         .toList();
+
     return Scaffold(
       appBar: AppBar(
         leading: Container(),
         title: const Text("タブ設定"),
         actions: [
           IconButton(
-              onPressed: () {
-                context.pushRoute(TabSettingsRoute());
-              },
-              icon: const Icon(Icons.add)),
+            onPressed: () {
+              context.pushRoute(TabSettingsRoute());
+            },
+            icon: const Icon(Icons.add),
+          ),
         ],
       ),
       body: Column(
@@ -44,48 +37,53 @@ class TabSettingsListPageState extends ConsumerState<TabSettingsListPage> {
         children: [
           Expanded(
             child: ReorderableListView.builder(
-                itemBuilder: (context, index) {
-                  final tabSetting = tabSettings[index];
-                  final account = ref.watch(accountProvider(tabSetting.acct));
-                  return ListTile(
-                    key: Key("$index"),
+              buildDefaultDragHandles: false,
+              itemCount: tabSettings.length,
+              itemBuilder: (context, index) {
+                final tabSetting = tabSettings[index];
+                final account = ref.watch(accountProvider(tabSetting.acct));
+                return ReorderableDragStartListener(
+                  key: Key("$index"),
+                  index: index,
+                  child: ListTile(
                     leading: AccountScope(
                       account: account,
-                      child: TabIconView(icon: tabSettings[index].icon),
+                      child: TabIconView(icon: tabSetting.icon),
                     ),
-                    title: Text(tabSettings[index].name),
+                    title: Text(tabSetting.name),
                     subtitle: Text(
                       "${tabSetting.tabType.displayName} / ${account.acct}",
                     ),
+                    trailing: const Icon(Icons.drag_handle),
                     onTap: () =>
                         context.pushRoute(TabSettingsRoute(tabIndex: index)),
-                  );
-                },
-                itemCount: tabSettings.length,
-                onReorder: (oldIndex, newIndex) {
-                  setState(() {
-                    if (oldIndex < newIndex) {
-                      newIndex -= 1;
-                    }
-                    final item = tabSettings.removeAt(oldIndex);
-                    tabSettings.insert(newIndex, item);
-                    save(tabSettings);
-                  });
-                }),
+                  ),
+                );
+              },
+              onReorder: (oldIndex, newIndex) {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final item = tabSettings.removeAt(oldIndex);
+                tabSettings.insert(newIndex, item);
+                ref.read(tabSettingsRepositoryProvider).save(tabSettings);
+              },
+            ),
           ),
           Align(
             alignment: Alignment.center,
             child: Padding(
               padding: const EdgeInsets.all(10),
               child: ElevatedButton(
-                  onPressed: () {
-                    context.router
-                      ..removeWhere((route) => true)
-                      ..push(const SplashRoute());
-                  },
-                  child: const Text("反映する")),
+                onPressed: () {
+                  context.router
+                    ..removeWhere((route) => true)
+                    ..push(const SplashRoute());
+                },
+                child: const Text("反映する"),
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
