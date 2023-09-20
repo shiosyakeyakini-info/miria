ca7ecb311f5f7cd5d37e6028512ae9974d281eff
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-09-21 00:39:40 +0900

    add trailing commas

--- lib/view/common/avatar_icon.dart
+++ lib/view/common/avatar_icon.dart
@@ -32,8 +32,10 @@ class AvatarIcon extends StatelessWidget {
     );
   }
 
-  factory AvatarIcon.fromUserResponse(UsersShowResponse response,
-      {double height = 48}) {
+  factory AvatarIcon.fromUserResponse(
+    UsersShowResponse response, {
+    double height = 48,
+  }) {
     return AvatarIcon(
       user: User(
         id: response.id,
@@ -49,72 +51,78 @@ class AvatarIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: onTap ??
-            () {
-              context.pushRoute(UserRoute(
-                  userId: user.id, account: AccountScope.of(context)));
-            },
-        child: Padding(
-            padding: const EdgeInsets.only(top: 3),
-            child: Stack(children: [
-              if (user.isCat)
-                Positioned(
-                    left: 0,
-                    top: 0,
-                    width: height * MediaQuery.of(context).textScaleFactor,
-                    height: height * MediaQuery.of(context).textScaleFactor,
-                    child: Transform.rotate(
-                      angle: -0 * pi / 180,
-                      child: Transform.translate(
-                        offset: Offset(
-                            -height *
-                                0.3 *
-                                MediaQuery.of(context).textScaleFactor,
-                            -height *
-                                0.3 *
-                                MediaQuery.of(context).textScaleFactor),
-                        child: Icon(Icons.play_arrow_rounded,
-                            color: Theme.of(context).primaryColor,
-                            size: height *
-                                1 *
-                                MediaQuery.of(context).textScaleFactor),
-                      ),
-                    )),
-              if (user.isCat)
-                Positioned(
-                    left: 0,
-                    top: 0,
-                    width: height * MediaQuery.of(context).textScaleFactor,
-                    height: height * MediaQuery.of(context).textScaleFactor,
-                    child: Transform.translate(
-                      offset: Offset(
-                          height *
-                              1.333 *
-                              MediaQuery.of(context).textScaleFactor,
-                          -height *
-                              0.3 *
-                              MediaQuery.of(context).textScaleFactor),
-                      child: Transform(
-                        transform: Matrix4.rotationY(pi),
-                        child: Icon(Icons.play_arrow_rounded,
-                            color: Theme.of(context).primaryColor,
-                            size: height *
-                                1 *
-                                MediaQuery.of(context).textScaleFactor),
-                      ),
-                    )),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(
-                    height * MediaQuery.of(context).textScaleFactor),
-                child: SizedBox(
-                    width: height * MediaQuery.of(context).textScaleFactor,
-                    height: height * MediaQuery.of(context).textScaleFactor,
-                    child: NetworkImageView(
-                      fit: BoxFit.cover,
-                      url: user.avatarUrl.toString(),
-                      type: ImageType.avatarIcon,
-                    )),
+      onTap: onTap ??
+          () {
+            context.pushRoute(
+              UserRoute(
+                userId: user.id,
+                account: AccountScope.of(context),
               ),
-            ])));
+            );
+          },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 3),
+        child: Stack(
+          children: [
+            if (user.isCat)
+              Positioned(
+                left: 0,
+                top: 0,
+                width: height * MediaQuery.of(context).textScaleFactor,
+                height: height * MediaQuery.of(context).textScaleFactor,
+                child: Transform.rotate(
+                  angle: -0 * pi / 180,
+                  child: Transform.translate(
+                    offset: Offset(
+                      -height * 0.3 * MediaQuery.of(context).textScaleFactor,
+                      -height * 0.3 * MediaQuery.of(context).textScaleFactor,
+                    ),
+                    child: Icon(
+                      Icons.play_arrow_rounded,
+                      color: Theme.of(context).primaryColor,
+                      size: height * 1 * MediaQuery.of(context).textScaleFactor,
+                    ),
+                  ),
+                ),
+              ),
+            if (user.isCat)
+              Positioned(
+                left: 0,
+                top: 0,
+                width: height * MediaQuery.of(context).textScaleFactor,
+                height: height * MediaQuery.of(context).textScaleFactor,
+                child: Transform.translate(
+                  offset: Offset(
+                    height * 1.333 * MediaQuery.of(context).textScaleFactor,
+                    -height * 0.3 * MediaQuery.of(context).textScaleFactor,
+                  ),
+                  child: Transform(
+                    transform: Matrix4.rotationY(pi),
+                    child: Icon(
+                      Icons.play_arrow_rounded,
+                      color: Theme.of(context).primaryColor,
+                      size: height * 1 * MediaQuery.of(context).textScaleFactor,
+                    ),
+                  ),
+                ),
+              ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(
+                height * MediaQuery.of(context).textScaleFactor,
+              ),
+              child: SizedBox(
+                width: height * MediaQuery.of(context).textScaleFactor,
+                height: height * MediaQuery.of(context).textScaleFactor,
+                child: NetworkImageView(
+                  fit: BoxFit.cover,
+                  url: user.avatarUrl.toString(),
+                  type: ImageType.avatarIcon,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
