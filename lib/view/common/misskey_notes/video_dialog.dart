513ed6d12392c7014ce1ef33beebbf85c9a715cc
4sterisk <4sterisk@ymail.ne.jp>
2024-01-11 18:12:49 +0900

    Dismissibleを使用するように

--- lib/view/common/misskey_notes/video_dialog.dart
+++ lib/view/common/misskey_notes/video_dialog.dart
@@ -14,12 +14,10 @@ class VideoDialog extends StatefulWidget {
 }
 
 class _VideoDialogState extends State<VideoDialog> {
+  late final videoKey = GlobalKey<VideoState>();
   late final player = Player();
   late final controller = VideoController(player);
   double aspectRatio = 1;
-  var verticalDragX = 0.0;
-  var verticalDragY = 0.0;
-  int? listeningId;
 
   @override
   void initState() {
@@ -45,11 +43,31 @@ class _VideoDialogState extends State<VideoDialog> {
   @override
   Widget build(BuildContext context) {
     final themeData = MaterialVideoControlsThemeData(
-      seekBarPositionColor: Theme.of(context).primaryColor,
-      seekBarThumbColor: Theme.of(context).primaryColor,
-      volumeGesture: false,
-      brightnessGesture: false,
-    );
+        seekBarPositionColor: Theme.of(context).primaryColor,
+        seekBarThumbColor: Theme.of(context).primaryColor,
+        backdropColor: Colors.transparent,
+        volumeGesture: false,
+        brightnessGesture: false);
+
+    final themeDataFull = MaterialVideoControlsThemeData(
+        seekBarPositionColor: Theme.of(context).primaryColor,
+        seekBarThumbColor: Theme.of(context).primaryColor,
+        volumeGesture: false,
+        brightnessGesture: false,
+        bottomButtonBarMargin:
+            const EdgeInsets.only(left: 16.0, right: 8.0, bottom: 16.0),
+        seekBarMargin: const EdgeInsets.only(bottom: 16.0));
+
+    final themeDataDesktop = MaterialDesktopVideoControlsThemeData(
+        seekBarPositionColor: Theme.of(context).primaryColor,
+        seekBarThumbColor: Theme.of(context).primaryColor,
+        modifyVolumeOnScroll: false);
+
+    final themeDataDesktopFull = MaterialDesktopVideoControlsThemeData(
+        seekBarPositionColor: Theme.of(context).primaryColor,
+        seekBarThumbColor: Theme.of(context).primaryColor,
+        modifyVolumeOnScroll: false);
+
     return AlertDialog(
         backgroundColor: Colors.transparent,
         titlePadding: EdgeInsets.zero,
@@ -57,79 +75,69 @@ class _VideoDialogState extends State<VideoDialog> {
         actionsPadding: EdgeInsets.zero,
         insetPadding: EdgeInsets.zero,
         content: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Stack(children: [
-              Positioned.fill(
-                child: Listener(
-                    onPointerDown: (event) {
-                      // プレイヤー操作できるように画面下部に不感エリア
-                      if (event.position.dy / MediaQuery.of(context).size.height > 0.85) {
-                        listeningId = null;
-                        return;
-                      }
-
-                      if (listeningId != null) {
-                        setState(() {
-                          verticalDragX = 0;
-                          verticalDragY = 0;
-                        });
-                        listeningId = null;
-                        return;
-                      }
-                      listeningId = event.pointer;
-                    },
-                    onPointerMove: (event) {
-                      if (listeningId != null) {
-                        setState(() {
-                          //verticalDragX += event.delta.dx;
-                          verticalDragY += event.delta.dy;
-                        });
-                      }
-                    },
-                    onPointerUp: (event) {
-                      final angle = (atan2(verticalDragY, verticalDragX).abs() /
-                          pi *
-                          180);
-                      if (listeningId != null &&
-                          verticalDragY.abs() > 10 &&
-                          (angle > 60 && angle < 120)) {
-                        Navigator.of(context).pop();
-                      } else {
-                        listeningId = null;
-                      }
-                    },
-                    child: Transform.translate(
-                        offset: Offset(verticalDragX, verticalDragY),
-                        child: MaterialVideoControlsTheme(
-                            normal: themeData,
-                            fullscreen: themeData,
-                            child: Video(controller: controller)))),
-              ),
-              Positioned(
-                left: 10,
-                top: 10,
-                child: RawMaterialButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    constraints:
-                        const BoxConstraints(minWidth: 0, minHeight: 0),
-                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    padding: EdgeInsets.zero,
-                    fillColor: Theme.of(context)
-                        .scaffoldBackgroundColor
-                        .withAlpha(300),
-                    shape: const CircleBorder(),
-                    child: Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Icon(Icons.close,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyMedium
-                                ?.color
-                                ?.withAlpha(500)))),
-              ),
-            ])));
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(children: [
+            Positioned.fill(
+              child: Dismissible(
+                key: const ValueKey(""),
+                behavior: HitTestBehavior.translucent,
+                direction: DismissDirection.vertical,
+                resizeDuration: null,
+                onDismissed: (_) => {Navigator.of(context).pop()},
+                child: Stack(children: [
+                  Align(
+                    child: AspectRatio(
+                      aspectRatio: aspectRatio,
+                      child: MaterialVideoControlsTheme(
+                        normal: themeData,
+                        fullscreen: themeDataFull,
+                        child: MaterialDesktopVideoControlsTheme(
+                          normal: themeDataDesktop,
+                          fullscreen: themeDataDesktopFull,
+                          child: Video(
+                            key: videoKey,
+                            controller: controller,
+                            controls: AdaptiveVideoControls,
+                            fill: Colors.transparent,
+                            onEnterFullscreen: () async {
+                              await defaultEnterNativeFullscreen();
+                              videoKey.currentState?.update(
+                                fill: Colors.black
+                                );
+                            },
+                            onExitFullscreen: () async {
+                              await defaultExitNativeFullscreen();
+                              videoKey.currentState?.update(
+                                fill: Colors.transparent
+                                );
+                            },
+                        )),
+                    ))),
+            Positioned(
+              left: 10,
+              top: 10,
+              child: RawMaterialButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                constraints: 
+                    const BoxConstraints(minWidth: 0, minHeight: 0),
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                padding: EdgeInsets.zero,
+                fillColor: Theme.of(context)
+                    .scaffoldBackgroundColor
+                    .withAlpha(200),
+                shape: const CircleBorder(),
+                child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Icon(Icons.close,
+                        color: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.color
+                            ?.withAlpha(200))))),
+             ])))
+      ])));
   }
 }
