9ee310e8519522ccb2b115add0649282e4de0392
cmt1910 <kerakera1910@gmail.com>
2024-01-11 22:06:25 +0900

    settings_pageにアイコンを追加

--- lib/view/settings_page/settings_page.dart
+++ lib/view/settings_page/settings_page.dart
@@ -14,22 +14,32 @@ class SettingsPage extends StatelessWidget {
         children: [
           ListTile(
             title: const Text("全般設定"),
+            leading: const Icon(Icons.settings),
+            trailing: const Icon(Icons.chevron_right),
             onTap: () => context.pushRoute(const GeneralSettingsRoute()),
           ),
           ListTile(
             title: const Text("アカウント設定"),
+            leading: const Icon(Icons.account_circle),
+            trailing: const Icon(Icons.chevron_right),
             onTap: () => context.pushRoute(const AccountListRoute()),
           ),
           ListTile(
             title: const Text("タブ設定"),
+            leading: const Icon(Icons.tab),
+            trailing: const Icon(Icons.chevron_right),
             onTap: () => context.pushRoute(const TabSettingsListRoute()),
           ),
           ListTile(
             title: const Text("設定のインポート・エクスポート"),
+            leading: const Icon(Icons.import_export),
+            trailing: const Icon(Icons.chevron_right),
             onTap: () => context.pushRoute(const ImportExportRoute()),
           ),
           ListTile(
             title: const Text("このアプリについて"),
+            leading: const Icon(Icons.info),
+            trailing: const Icon(Icons.chevron_right),
             onTap: () => context.pushRoute(const AppInfoRoute()),
           )
         ],
