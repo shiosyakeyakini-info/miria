63bea8b8415801c59228e4b0da3e14c9f6aa7fc4
sorairo <sorairo@shiosyakeyakini.info>
2024-02-04 22:58:56 +0900

    ShareExtensionから共有したものを読み出せるところまで

--- lib/view/share_extension_page/share_extension_page.dart
+++ lib/view/share_extension_page/share_extension_page.dart
@@ -4,32 +4,49 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preference_app_group/shared_preference_app_group.dart';
 
 @RoutePage()
-class ShareExtensionPage extends ConsumerWidget {
+class ShareExtensionPage extends ConsumerStatefulWidget {
   const ShareExtensionPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      ShareExtensionPageState();
+}
+
+class ShareExtensionPageState extends ConsumerState<ShareExtensionPage> {
+  var sharedPreference = "";
+  var sharedData = "";
+  String? error;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Future(() async {
+      try {
+        await SharedPreferenceAppGroup.setAppGroup(
+            "group.info.shiosyakeyakini.miria");
+        sharedPreference =
+            await SharedPreferenceAppGroup.get("account_settings") as String? ??
+                "";
+        sharedData =
+            await SharedPreferenceAppGroup.get("ShareKey") as String? ?? "";
+        await SharedPreferenceAppGroup.setString("ShareKey", "");
+        setState(() {});
+      } catch (e) {
+        setState(() {
+          error = e.toString();
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Share Extension Page Test"),
       ),
-      body: FutureBuilder(
-        future: Future(() async {
-          await SharedPreferenceAppGroup.setAppGroup(
-              "group.info.shiosyakeyakini.miria");
-          return await SharedPreferenceAppGroup.get("account_settings")
-              as String?;
-        }),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Text(snapshot.requireData ?? "no data");
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}\n${snapshot.stackTrace}");
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
-      ),
+      body: Text(error ??
+          "SharedPreference: $sharedPreference\n共有されたデータ: $sharedData"),
     );
   }
 }
