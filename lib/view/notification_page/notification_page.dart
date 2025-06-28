41a6ef531da7de18fecb9d59ec4c9a4ee5e82da3
sorairo <sorairo@shiosyakeyakini.info>
2025-06-28 19:39:09 +0900

    通知ページにおけるフォロワーからのメッセージをアイコンより右に移動するように

--- lib/view/notification_page/notification_page.dart
+++ lib/view/notification_page/notification_page.dart
@@ -416,8 +416,11 @@ class NotificationItem extends ConsumerWidget {
                     orElse: () => Container(),
                   ),
               if (type is FollowRequestAccepted && type.message != null)
-                SimpleMfmText(
-                  S.of(context).messageForFollower(type.message ?? ""),
+                Padding(
+                  padding: const EdgeInsets.only(left: 85),
+                  child: SimpleMfmText(
+                    S.of(context).messageForFollower(type.message ?? ""),
+                  ),
                 ),
             ],
           ),
