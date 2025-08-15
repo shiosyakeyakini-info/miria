3da4293675b12325f4ff7f368f1fe9ebbfb26d26
4sterisk <4sterisk@ymail.ne.jp>
2025-08-16 02:55:19 +0900

    chore: ルーム削除完了ダイアログを削除

--- lib/view/chat_page/room_info.dart
+++ lib/view/chat_page/room_info.dart
@@ -155,11 +155,6 @@ class ChatRoomInfo extends HookConsumerWidget {
                             .chat
                             .rooms
                             .delete(ChatRoomsDeleteRequest(roomId: room.id));
-                        await ref
-                            .read(dialogStateNotifierProvider.notifier)
-                            .showSimpleDialog(
-                              message: (context) => S.of(context).delete,
-                            );
                         if (!context.mounted) return;
                         // ルーム削除後は現在のルームチャット画面を削除して前の画面に戻る
                         // まずDrawerを閉じてからナビゲーションを実行
